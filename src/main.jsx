3265a72ddbf6e36bf898bf981a2243a75f3d92f2
Shubhangi Borikar <shubhangiborikar@gmail.com>
2025-04-01 14:22:30 -0700

    🎨 Initial pastel portfolio commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './style.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>DATA LOOM</h1>
+      <p><em>Where stories are woven from data ✨</em></p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
