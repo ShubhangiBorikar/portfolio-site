25173e88da5f016039a76e0ab1323274a54aabac
Shubhangi Borikar <shubhangiborikar@gmail.com>
2025-04-01 13:10:38 -0700

    🎨 Initial pastel portfolio commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,7 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {
-      colors: {
-        fairyblue: "#e5f0fb",
-        lavender: "#f6f6ff",
-        softgray: "#f9f9f9"
-      },
-      fontFamily: {
-        sans: ["'Segoe UI'", "sans-serif"]
-      }
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
